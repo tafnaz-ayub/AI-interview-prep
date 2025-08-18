e950ea9481b6ed9a0e72c1b8a186d32e9efba767
tafnaz-ayub <ayubtafnaz@gmail.com>
2025-08-18 22:56:54 +0530

    Authentication APIs

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 // Middleware to handle CORS
@@ -14,10 +15,19 @@ app.use(
   })
 );
 
+connectDB()
 // Middleware
 app.use(express.json());
 
 // Routes
+app.use("/api/auth", authRoutes);
+
+// app.use('/api/sessions', sessionRoutes);
+// app.use('/api/questions', questionRoutes);
+
+
+// app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
