388997bc28131a27aef0bc9c27986d2363e73627
tafnaz-ayub <ayubtafnaz@gmail.com>
2025-08-19 23:16:39 +0530

    summary card

--- frontend/ai-interview-prep/src/Pages/Home/Dashboard.jsx
+++ frontend/ai-interview-prep/src/Pages/Home/Dashboard.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import axiosInstance from "../../utils/axioInstance";
 import { API_PATHS } from "../../utils/apiPaths";
 import SummaryCard from '../../components/Cards/SummaryCard';
+import moment from 'moment';
 const Dashboard = () => {
   const navigate = useNavigate();
 
